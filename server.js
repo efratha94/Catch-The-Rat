49c01913f0199d402a430c0f64e63d590190a9a6
Efrat Harel <efratha94@gmail.com>
2019-12-29 20:54:24 +0200

    Catch The Rat

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const path = require("path")
-const port = 3009
+const PORT = 3009
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
@@ -10,6 +10,6 @@ app.use(express.static(path.join(__dirname, "node_modules")))
 app.use(express.static(path.join(__dirname, "dist")))
 
 
-app.listen(port, function(){
-    console.log("Listening on port "+port)
+app.listen(process.env.PORT || PORT, function(){
+    console.log("Listening on port "+ PORT)
 })
